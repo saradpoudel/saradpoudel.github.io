c9d0c3cf4c8a6063711649e6336c41104e2e51cd
sarad poudel <saradpoudel05@gmail.com>
2024-09-14 16:14:43 +1000

    Update the bugs

--- src/constants/index.js
+++ src/constants/index.js
@@ -124,7 +124,7 @@ const experiences = [
   {
     title: "Full Stack Developer",
     company_name: "Skillup Labs",
-    icon: "https://skilluplabs.com.au/assets/images/logo_horizontal.png",
+    icon: "https://www.skilluplabs.com.au/skilluplabs_logo.jpg",
     iconBg: "#E6DEDD",
     date: "Jan 2023 - Sep 2023",
     points: [
